790ebd84c6f5ad9e5051cbf7140b812215cbe521
miintory <mrk3282@gmail.com>
2022-12-15 16:53:05 +0900

    포폴 4:53

--- common.js
+++ common.js
@@ -1,10 +1,36 @@
+const $bell = document.querySelector(".bells"),
+  $sub = document.querySelector(".sec1_sub"),
+  $sec1 = document.querySelector(".sec1"),
+  $secMain = document.querySelector(".sec1_main"),
+  $rad = document.querySelector(".radius");
+
+$sec1.addEventListener("click", function () {
+  $sub.classList.add("on");
+  $secMain.classList.add("on");
+  $rad.classList.add("on");
+});
+
 var swiper = new Swiper(".scroll", {
   direction: "vertical",
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 0,
   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
+function clickEffect(e) {
+  var d = document.createElement("div");
+  d.className = "clickEffect";
+  d.style.top = e.clientY + "px";
+  d.style.left = e.clientX + "px";
+  document.body.appendChild(d);
+  d.addEventListener(
+    "animationend",
+    function () {
+      d.parentElement.removeChild(d);
+    }.bind(this)
+  );
+}
+document.addEventListener("click", clickEffect);
